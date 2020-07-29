1e1927d3924e25a5e89db2f2da1fff8a459f6fb6
Jhonatan De Queiroz Silva <jhonatan.de.queiroz.silva@ibm.com>
2020-07-29 14:52:10 -0400

    Add Event section

--- src/features/eventDashboard/EventDashboard.jsx
+++ src/features/eventDashboard/EventDashboard.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import EventList from './EventList';
+import EventForm from '../eventForm/EventForm';
 
 export default function EventDashboard() {
     return (
     <Grid>
         <Grid.Column width={10}>
-            <h2>Left Column</h2>
+            <EventList/>
         </Grid.Column>
         <Grid.Column width={6}>
-            <h2>Right Column</h2>
+            <EventForm/>
         </Grid.Column>
     </Grid>
     )
