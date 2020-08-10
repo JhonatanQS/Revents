067d7dd8b1e1e0115091874d330be06d57447183
Jhonatan De Queiroz Silva <jhonatan.de.queiroz.silva@ibm.com>
2020-08-10 10:22:17 -0400

    Add hide/show form

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import EventDashboard from "../../features/eventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar";
 import { Container } from "semantic-ui-react";
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
     <>
-      <NavBar />
+      <NavBar setFormOpen={setFormOpen}/>
       <Container className='main'>
-        <EventDashboard />
+        <EventDashboard formOpen={formOpen}  setFormOpen={setFormOpen}/>
       </Container>
     </>
   );
