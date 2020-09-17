7efc449989debe628e1895779e4415c52b628eb6
Jhonatan De Queiroz Silva <jhonatan.de.queiroz.silva@ibm.com>
2020-09-17 13:21:02 -0400

    Add Profile section

--- src/features/auth/AccountPage.jsx
+++ src/features/auth/AccountPage.jsx
@@ -11,96 +11,92 @@ import { toast } from "react-toastify";
 export default function AccountPage() {
   const { currentUser } = useSelector((state) => state.auth);
   return (
-    <Grid centered>
-      <Grid.Column width={12}>
-        <Segment>
-          <Header dividing size='large' content='Account' />
-          {currentUser.providerId === "password" && (
-            <>
-              <Header color='teal' sub content='Change Password' />
-              <p>Use this from to change your Password</p>
-              <Formik
-                initialValues={{ newPassword1: "", newPassword2: "" }}
-                validationSchema={Yup.object({
-                  newPassword1: Yup.string().required("Password is required"),
-                  newPassword2: Yup.string().oneOf(
-                    [Yup.ref("newPassword1"), null],
-                    "Passwords do not match"
-                  ),
-                })}
-                onSubmit={async (values, { setSubmitting, setErrors }) => {
-                  try {
-                    await updateUserPassword(values);
-                    toast.success('Password was successfully changed')
-                  } catch (error) {
-                    setErrors({ auth: error.message });
-                  } finally {
-                    setSubmitting(false);
-                  }
-                }}
-              >
-                {({ errors, isSubmitting, isValid, dirty }) => (
-                  <Form className='ui form'>
-                    <MyTextInput
-                      name='newPassword1'
-                      type='password'
-                      placeholder='New Password'
-                    />
-                    <MyTextInput
-                      name='newPassword2'
-                      type='password'
-                      placeholder='Confirm Password'
-                    />
-                    {errors.auth && (
-                      <Label
-                        basic
-                        color='red'
-                        style={{ marginBottom: 10 }}
-                        content={errors.auth}
-                      />
-                    )}
-                    <Button
-                      style={{ display: "block" }}
-                      type='submit'
-                      disabled={!isValid || isSubmitting || !dirty}
-                      loading={isSubmitting}
-                      size='large'
-                      positive
-                      content='Update password'
-                    />
-                  </Form>
+    <Segment>
+      <Header dividing size='large' content='Account' />
+      {currentUser.providerId === "password" && (
+        <>
+          <Header color='teal' sub content='Change Password' />
+          <p>Use this from to change your Password</p>
+          <Formik
+            initialValues={{ newPassword1: "", newPassword2: "" }}
+            validationSchema={Yup.object({
+              newPassword1: Yup.string().required("Password is required"),
+              newPassword2: Yup.string().oneOf(
+                [Yup.ref("newPassword1"), null],
+                "Passwords do not match"
+              ),
+            })}
+            onSubmit={async (values, { setSubmitting, setErrors }) => {
+              try {
+                await updateUserPassword(values);
+                toast.success("Password was successfully changed");
+              } catch (error) {
+                setErrors({ auth: error.message });
+              } finally {
+                setSubmitting(false);
+              }
+            }}
+          >
+            {({ errors, isSubmitting, isValid, dirty }) => (
+              <Form className='ui form'>
+                <MyTextInput
+                  name='newPassword1'
+                  type='password'
+                  placeholder='New Password'
+                />
+                <MyTextInput
+                  name='newPassword2'
+                  type='password'
+                  placeholder='Confirm Password'
+                />
+                {errors.auth && (
+                  <Label
+                    basic
+                    color='red'
+                    style={{ marginBottom: 10 }}
+                    content={errors.auth}
+                  />
                 )}
-              </Formik>
-            </>
-          )}
-          {currentUser.providerId === "facebook.com" && (
-            <>
-              <Header color='teal' sub content='Facebook account' />
-              <p>Please visit Facebook to update your account</p>
-              <Button
-                icon='facebook'
-                color='facebook'
-                as={Link}
-                to='https://facebook.com'
-                content='Go to Facebook'
-              />
-            </>
-          )}
-          {currentUser.providerId === "google.com" && (
-            <>
-              <Header color='teal' sub content='Google account' />
-              <p>Please visit Google to update your account</p>
-              <Button
-                icon='google'
-                color='google plus'
-                as={Link}
-                to='https://google.com'
-                content='Go to Google'
-              />
-            </>
-          )}
-        </Segment>
-      </Grid.Column>
-    </Grid>
+                <Button
+                  style={{ display: "block" }}
+                  type='submit'
+                  disabled={!isValid || isSubmitting || !dirty}
+                  loading={isSubmitting}
+                  size='large'
+                  positive
+                  content='Update password'
+                />
+              </Form>
+            )}
+          </Formik>
+        </>
+      )}
+      {currentUser.providerId === "facebook.com" && (
+        <>
+          <Header color='teal' sub content='Facebook account' />
+          <p>Please visit Facebook to update your account</p>
+          <Button
+            icon='facebook'
+            color='facebook'
+            as={Link}
+            to='https://facebook.com'
+            content='Go to Facebook'
+          />
+        </>
+      )}
+      {currentUser.providerId === "google.com" && (
+        <>
+          <Header color='teal' sub content='Google account' />
+          <p>Please visit Google to update your account</p>
+          <Button
+            icon='google'
+            color='google plus'
+            as={Link}
+            to='https://google.com'
+            content='Go to Google'
+          />
+        </>
+      )}
+    </Segment>
   );
 }
